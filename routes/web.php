199eeb7e2ab5cd508c8572809dc6eb8765dc7523
Fernando Parada <fernandoparada18@protonmail.com>
2023-07-18 19:34:16 -0400

    test de navbar con tailwindcss

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome')->name('home');
+
+Route::view('/contacto', 'contact')->name('contact');
+
+Route::view('/blog', 'blog')->name('blog');
+
+Route::view('/about', 'about')->name('about');
