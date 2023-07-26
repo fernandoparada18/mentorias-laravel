fea90582d28100c324dadaa41b0e1cce14304d1c
Fernando Parada <fernandoparada18@protonmail.com>
2023-07-26 00:14:18 -0400

    Cuarta clase

--- database/migrations/2023_07_24_183521_create_pages_table.php
+++ database/migrations/2023_07_24_183521_create_pages_table.php
@@ -10,9 +10,11 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('pages', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->text('content');
+            $table->string('slug');
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('pages');
     }
 };
